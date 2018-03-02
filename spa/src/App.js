06809c15e71bdd2364a1007510fdc0f0f39ba69b
Your Name <karoliina.mietola@helsinki.fi>
2018-03-03 00:30:17 +0200

    muutoksia

--- spa/src/App.js
+++ spa/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Note from './components/Note'
 import noteService from './services/notes'
+import loginService from './services/login'
 
 const Notification =({message}) => {
   if(message===null) {
@@ -13,6 +14,74 @@ const Notification =({message}) => {
   )
 }
 
+const LoginForm=({handleSubmit, handleChange, username, password})=> {
+  return(
+  <div>
+    <h2>Kirjaudu</h2>
+    <form onSubmit={handleSubmit}>
+      <div>
+        käyttäjätunnus
+        <input
+          value={username}
+          onChange={handleChange}
+          name="username"
+        />
+      </div>
+      <div>
+        salasana
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit">kirjaudu</button>
+    </form>
+  </div>
+)}
+const NoteForm=({onSubmit, handleChange, value})=> {
+  return (
+  <div>
+    <h2>Luo uusi muistiinpano</h2>
+
+    <form onSubmit={onSubmit}>
+      <input
+        value={value}
+        onChange={handleChange}
+      />
+      <button type="submit">tallenna</button>
+    </form>
+  </div>
+  )
+}
+class Togglable extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      visible:false
+    }
+  }
+  toggleVisibility=()=>{
+    this.setState({visible: !this.state.visible})
+  }
+  render() {
+    const hideWhenVisible={ display: this.state.visible ? 'none' : '' }
+    const showWhenVisible={ display: this.state.visible ? '' : 'none' }
+
+    return(
+      <div>
+        <div style={hideWhenVisible}>
+          <button onClick={this.toggleVisibility}>{this.props.buttonLabel}</button>
+        </div>
+        <div style={showWhenVisible}>
+          {this.props.children}
+          <button onClick={this.toggleVisibility}>cancel</button>
+        </div>
+      </div>
+    )
+  }
+}
 class App extends React.Component {
   constructor(props) {
     super(props)
@@ -20,23 +89,34 @@ class App extends React.Component {
       notes: [],
       newNote: '',
       showAll: true,
-      error: null
+      error: null,
+      username: '',
+      password: '',
+      user: null,
+      loginVisible: false
     }
   }
   componentDidMount() {
     noteService
       .getAll()
-      .then(response => {
-        this.setState({notes: response})
+      .then(notes => {
+        this.setState({notes})
       })
+      const loggedUserJSON=window.localStorage.getItem('loggedNoteappUser')
+      if (loggedUserJSON) {
+        const user=JSON.parse(loggedUserJSON)
+        this.setState({user})
+        noteService.setToken(user.token)
+      }
   }
 
 
   addNote = (event) => {
     event.preventDefault()
+    this.noteForm.toggleVisibility()
     const noteObject = {
       content: this.state.newNote,
-      date: new Date().new,
+      date: new Date(),
       important: Math.random() > 0.5
     }
     noteService
@@ -49,10 +129,33 @@ class App extends React.Component {
       })
 
   }
+  login= async (event)=> {
+    event.preventDefault()
+    try{
+      const user=await loginService.login({
+        username:this.state.username,
+        password:this.state.password
+      })
+      window.localStorage.setItem('loggedNoteappUser', JSON.stringify(user))
+      noteService.setToken(user.token)
+      this.setState({username:'', password:'', user})
+    } catch(exception) {
+      this.setState({
+        error: 'Käyttäjätunnus tai salasana virheellinen'
+      })
+      setTimeout(()=> {
+        this.setState({error:null})
+      }, 5000)
+    }
+  }
 handleNoteChange = (event) => {
   console.log(event.target.value)
   this.setState({newNote: event.target.value})
 }
+handleLoginFieldChange=(event)=>{
+  this.setState({[event.target.name]:event.target.value})
+}
+
 toggleVisible = () => {
   this.setState({showAll: !this.state.showAll})
 }
@@ -82,17 +185,57 @@ toggleImportanceOf = (id) => {
 }
 
 render() {
+
+
+
   const notesToShow =
     this.state.showAll ?
       this.state.notes :
       this.state.notes.filter(note => note.important===true)
 
   const label = this.state.showAll ? 'vain tärkeät' : 'kaikki'
+  const loginForm=()=>{
+    return(
+    <div>
+    <Togglable buttonLabel="login">
+      <LoginForm
+        visible={this.state.visible}
+        username={this.state.username}
+        password={this.state.password}
+        handleChange={this.handleLoginFieldChange}
+        handleSubmit={this.login}
+      />
+    </Togglable>
+    </div>
+    )
+  }
+  const noteForm=()=>{
+    return(
+      <div>
+        <Togglable buttonLabel="new note" ref={component => this.noteForm = component}>
+          <NoteForm
+            onSubmit={this.addNote}
+            value={this.state.newNote}
+            handleChange={this.handleNoteChange}
+          />
+        </Togglable>
+      </div>
+    )
+  }
 
   return (
     <div>
+
       <h1>Muistiinpanot</h1>
       <Notification message={this.state.error} />
+      {this.state.user === null ?
+        loginForm() :
+        <div>
+          <p>{this.state.user.name} logged in </p>
+          {noteForm()}
+        </div>
+      }
+      <h2>muistiinpanot</h2>
       <div>
         <button onClick={this.toggleVisible}>
         näytä {label}
@@ -106,13 +249,7 @@ render() {
             toggleImportance={this.toggleImportanceOf(note.id)}
             />)}
       </ul>
-      <form onSubmit={this.addNote}>
-        <input
-          value={this.state.newNote}
-          onChange={this.handleNoteChange}
-        />
-        <button type="submit">tallenna</button>
-      </form>
+
     </div>
   )
 }
